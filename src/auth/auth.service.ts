a4a6de2a4eabf862cab019d73e5575c8d86dccf1
Omar Baabouj <baabouj.dev@gmail.com>
2022-04-26 14:05:56 +0000

    :sparkles: NEW: implement signup

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,8 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { SignupDto } from './dto';
 
 @Injectable()
 export class AuthService {
@@ -9,7 +12,29 @@ export class AuthService {
     // TODO: Implement login
   }
 
-  signup() {
-    // TODO: Implement signup
+  async signup({ name, email, password }: SignupDto) {
+    try {
+      const hashedPassword = await argon.hash(password);
+      const user = await this.prismaService.user.create({
+        data: {
+          name,
+          email,
+          password: hashedPassword,
+        },
+      });
+
+      // TODO: return a JWT, holding the user id, instead of the user object
+
+      delete user.password;
+      return user;
+    } catch (error) {
+      // Check if the error is a unique constraint violation
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials already taken');
+        }
+      }
+      throw error;
+    }
   }
 }
