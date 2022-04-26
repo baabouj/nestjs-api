2fd22fa6a9da0bb7d9cbf1d853200d990206cbf6
Omar Baabouj <baabouj.dev@gmail.com>
2022-04-26 11:05:10 +0000

    :ok_hand: IMPROVE: validation

--- src/auth/dto/login.dto.ts
+++ src/auth/dto/login.dto.ts
@@ -1,4 +1,11 @@
-export interface LoginDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class LoginDto {
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
--- src/auth/dto/signup.dto.ts
+++ src/auth/dto/signup.dto.ts
@@ -1,5 +1,15 @@
-export interface SignupDto {
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class SignupDto {
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsNotEmpty()
+  @IsString()
   password: string;
 }
